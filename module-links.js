b236c54ed14d11fe9dd0b70fd6a00fb42138a032
Tashinga Mtoko <54450330+tasmto@users.noreply.github.com>
2020-02-16 09:08:41 +0200

    Add files via upload

---
+++ module-links.js
@@ -0,0 +1,14 @@
+
+function module0() {
+        window.open('https://umich.instructure.com/courses/368871/modules/items/923677','_blank');
+}
+
+function module1() {
+    window.open('https://umich.instructure.com/courses/368871/modules/items/924294','_blank');
+}
+
+function module2() {
+    window.open('https://umich.instructure.com/courses/368871/modules/items/915110','_blank');
+}
+
+
